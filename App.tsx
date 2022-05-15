50c2c7cc3c8210bda45dc33c6d9017aab8b59d0b
Yi Guo <guoyi0328@gmail.com>
2022-05-15 01:20:01 -0700

    added native-base

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView, View } from "react-native";
-import BottomNavigation from "./components/BottomNavigation";
-import ShuffleScreen from "./screens/ShuffleScreen";
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+// import BottomNavigation from "./components/BottomNavigation";
+// import ShuffleScreen from "./screens/ShuffleScreen";
+import { NativeBaseProvider, Box } from "native-base";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.screen}>
-        <ShuffleScreen />
-      </View>
-      <View style={styles.bottomNavigation}>
-        <BottomNavigation />
-      </View>
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <Box>Hello World!</Box>
+    </NativeBaseProvider>
+    // <SafeAreaView style={styles.container}>
+    //   <View style={styles.screen}>
+    //     <ShuffleScreen />
+    //   </View>
+    //   <View style={styles.bottomNavigation}>
+    //     <BottomNavigation />
+    //   </View>
+    // </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flexGrow: 1 },
-  screen: { flexGrow: 1 },
-  bottomNavigation: { flexGrow: 0 },
-});
+// const styles = StyleSheet.create({
+//   container: { flexGrow: 1 },
+//   screen: { flexGrow: 1 },
+//   bottomNavigation: { flexGrow: 0 },
+// });
